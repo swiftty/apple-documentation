8f86bee8f22584398a8895fb297776472b720ac9
swiftty <62803132+swiftty@users.noreply.github.com>
2023-09-14 21:11:58 +0900

    add layout when pressing cell

--- AppleDocumentationPackage/Sources/App/Pages/AllTechnologiesPage/AllTechnologiesPage.swift
+++ AppleDocumentationPackage/Sources/App/Pages/AllTechnologiesPage/AllTechnologiesPage.swift
@@ -5,80 +5,37 @@ import AppleDocClient
 public struct AllTechnologiesPage: View {
     @Environment(\.appleDocClient) var appleDocClient
 
-    @State private var allTechnologies: [Technology] = []
+    @State private var allTechnologies: [Technology]?
     @State private var filterText = ""
 
     private var technologies: [Technology] {
         if filterText.isEmpty {
-            allTechnologies
+            allTechnologies ?? []
         } else {
-            allTechnologies
+            allTechnologies?
                 .filter { $0.title.contains(filterText) }
+            ?? []
         }
+    }
+
+    private var isLoading: Bool {
+        allTechnologies == nil
     }
 
     public init() {}
 
     public var body: some View {
         ScrollView {
-            LazyVStack(spacing: 0, pinnedViews: .sectionHeaders) {
-                Section {
-                    ForEach(technologies, id: \.title) { tech in
-                        NavigationLink(value: tech.destination) {
-                            TechnologyCell(
-                                title: tech.title,
-                                abstract: tech.destination.abstract
-                            )
-                        }
-                        .buttonStyle(TechnologyCellButtonStyle())
-                        .padding(.horizontal)
-                        .padding(.top)
-                    }
-                } header: {
-                    if !technologies.isEmpty {
-                        Text("Showing \(technologies.count) results")
-                            .font(.caption2.bold())
-                            .foregroundStyle(.secondary)
-                            .padding(.horizontal, 8)
-                            .padding(.vertical, 4)
-                            .background(.thinMaterial, in: Capsule())
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .padding(.leading)
-                            .padding(.top, 4)
-                    }
-                }
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(.circular)
+            } else {
+                listContent()
             }
         }
         .overlay {
             if technologies.isEmpty {
-                ContentUnavailableView {
-                    VStack(alignment: .leading, spacing: 16) {
-                        Text("No results found")
-                            .font(.caption2.bold())
-                            .foregroundStyle(.secondary)
-                            .padding(.top)
-
-                        Text("No results found Try changing or removing text.")
-                            .font(.body)
-                            .foregroundStyle(.primary)
-                            .padding()
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background {
-                                RoundedRectangle(cornerRadius: 12, style: .continuous)
-                                    .stroke(
-                                        .secondary,
-                                        style: StrokeStyle(
-                                            lineWidth: 1,
-                                            dash: [8, 4]
-                                        )
-                                    )
-                            }
-
-                        Spacer()
-                    }
-                }
-                .padding(-16)
+                emptyContent()
             }
         }
         .searchable(
@@ -93,11 +50,66 @@ public struct AllTechnologiesPage: View {
             } catch {}
         }
     }
-}
 
-private struct TechnologyCellButtonStyle: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
+    private func listContent() -> some View {
+        LazyVStack(spacing: 0, pinnedViews: .sectionHeaders) {
+            Section {
+                ForEach(technologies, id: \.title) { tech in
+                    NavigationLink(value: tech.destination) {
+                        TechnologyCell(
+                            title: tech.title,
+                            abstract: tech.destination.abstract
+                        )
+                    }
+                    .buttonStyle(TechnologyCell.ButtonStyle())
+                    .padding(.horizontal)
+                    .padding(.top)
+                }
+            } header: {
+                if !technologies.isEmpty {
+                    Text("Showing \(technologies.count) results")
+                        .font(.caption2.bold())
+                        .foregroundStyle(.secondary)
+                        .padding(.horizontal, 8)
+                        .padding(.vertical, 4)
+                        .background(.thinMaterial, in: Capsule())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading)
+                        .padding(.top, 4)
+                }
+            }
+        }
+    }
+
+    private func emptyContent() -> some View {
+        ContentUnavailableView {
+            VStack(alignment: .leading, spacing: 16) {
+                Text("No results found")
+                    .font(.caption2.bold())
+                    .foregroundStyle(.secondary)
+                    .padding(.top)
+
+                Text("No results found Try changing or removing text.")
+                    .font(.body)
+                    .foregroundStyle(.primary)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 12, style: .continuous)
+                            .stroke(
+                                .secondary,
+                                style: StrokeStyle(
+                                    lineWidth: 1,
+                                    dash: [8, 4]
+                                )
+                            )
+                    }
+
+                Spacer()
+            }
+        }
+        .padding(-16)
     }
 }
 
--- AppleDocumentationPackage/Sources/App/Pages/AllTechnologiesPage/Views/TechnologyCell.swift
+++ AppleDocumentationPackage/Sources/App/Pages/AllTechnologiesPage/Views/TechnologyCell.swift
@@ -1,26 +1,57 @@
 import SwiftUI
 
 struct TechnologyCell: View {
+    @Environment(\.isPressed) var isPressed
+
     var title: String
     var abstract: String
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(title)
-                .font(.headline.bold())
-                .foregroundStyle(.primary)
+        HStack(spacing: 0) {
+            VStack(alignment: .leading) {
+                Text(title)
+                    .font(.headline.bold())
+                    .foregroundStyle(.primary)
 
-            Text(abstract)
-                .font(.footnote)
-                .foregroundStyle(.secondary)
+                Text(abstract)
+                    .font(.footnote)
+                    .foregroundStyle(.secondary)
+            }
+
+            Spacer()
+
+            Image(systemName: "chevron.right")
+                .font(.body)
+                .foregroundStyle(.blue)
         }
         .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
         .background {
             RoundedRectangle(cornerRadius: 12, style: .continuous)
                 .stroke(lineWidth: 1)
-                .fill(.secondary)
+                .fill(isPressed ? .blue : .secondary)
         }
+    }
+}
+
+extension TechnologyCell {
+    struct ButtonStyle: SwiftUI.ButtonStyle {
+       func makeBody(configuration: Configuration) -> some View {
+           configuration.label
+               .environment(\.isPressed, configuration.isPressed)
+               .scaleEffect(configuration.isPressed ? 1.02 : 1)
+       }
+   }
+}
+
+extension EnvironmentValues {
+    private struct Key: EnvironmentKey {
+        static var defaultValue: Bool = false
+    }
+
+    var isPressed: Bool {
+        get { self[Key.self] }
+        set { self[Key.self] = newValue }
     }
 }
 
