d7f276ca5f4f350ccc9c61b9340c078994fc9386
swiftty <62803132+swiftty@users.noreply.github.com>
2025-04-18 17:20:25 +0900

    Apply code reviews

--- AppleDocumentationPackage/Tests/AppleDocumentationAPITests/TechnologyDetailTests.swift
+++ AppleDocumentationPackage/Tests/AppleDocumentationAPITests/TechnologyDetailTests.swift
@@ -9,9 +9,9 @@ struct TechnologyDetailTests {
         let url = try #require(URL(string: "https://developer.apple.com/tutorials/data/documentation/coretext.json"))
         let (data, _) = try await URLSession.shared.data(from: url)
 
-        #expect(throws: Never.self, performing: {
+        #expect(throws: Never.self) {
             try decodeTechnologyDetail(from: data)
-        })
+        }
     }
 
     @Test
@@ -19,9 +19,9 @@ struct TechnologyDetailTests {
         let url = try #require(URL(string: "https://developer.apple.com/tutorials/data/documentation/coretext/ctfont.json"))
         let (data, _) = try await URLSession.shared.data(from: url)
 
-        #expect(throws: Never.self, performing: {
+        #expect(throws: Never.self) {
             try decodeTechnologyDetail(from: data)
-        })
+        }
     }
 
     @Test
@@ -29,8 +29,8 @@ struct TechnologyDetailTests {
         let url = try #require(URL(string: "https://developer.apple.com/tutorials/data/documentation/appstoreconnectapi.json"))
         let (data, _) = try await URLSession.shared.data(from: url)
 
-        #expect(throws: Never.self, performing: {
+        #expect(throws: Never.self) {
             try decodeTechnologyDetail(from: data)
-        })
+        }
     }
 }
